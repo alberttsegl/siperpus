d105d9cc36aeb0591d35589d052197e710e3cc29
alberttsegl <albertseptiangulo8@gmail.com>
2026-02-25 09:22:28 +0700

    Memperbaiki header tabel

--- resources/views/books/tabel.blade.php
+++ resources/views/books/tabel.blade.php
@@ -4,7 +4,7 @@
 <div class="row">
   <div class="col-12">
     <div class="card mb-4">
-      <div class="card-header pb-0 d-flex justify-content-between align-items-center">
+      <div class="card-header pb-0 d-flex justify-content-between align-items-center ps-3">
         <h6>Projects Table</h6>
         <input type="text" id="bookSearch" class="form-control form-control-sm w-auto" placeholder="Search books...">
       </div>
