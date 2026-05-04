ad78eef052f5ef9bafb2da070e7fde36f0f40fd0
alberttsegl <albertseptiangulo8@gmail.com>
2026-05-05 06:39:44 +0700

    Menghapus icon sign in pada be/master.blade.php

--- resources/views/be/master.blade.php
+++ resources/views/be/master.blade.php
@@ -70,8 +70,6 @@
     @if (!Route::is('login'))
         <li class="nav-item d-flex align-items-center">
             <a href="{{ route('login') }}" class="nav-link text-body font-weight-bold px-0">
-                <i class="fa fa-user me-sm-1"></i>
-                <span class="d-sm-inline d-none">Sign In</span>
             </a>
         </li>
     @endif
